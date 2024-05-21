c6b760a5d3b4464012184785bc73ce2316b057c4
w30029682 <wangjinfan1@huawei.com>
2024-05-21 20:25:49 +0800

    #feat remove operator

--- include/vm/operator/add.h
+++
@@ -1,7 +0,0 @@
-#ifndef __NEO_VM_OPERATOR_ADD__
-#define __NEO_VM_OPERATOR_ADD__
-
-#include "engine/type.h"
-neo_value neo_vm_opt_add(neo_context ctx, uint32_t opt, int argc,
-                         neo_value *argv);
-#endif
--- src/vm/operator/convert.c
+++
@@ -1,8 +0,0 @@
-#include "vm/operator/convert.h"
-#include "engine/type.h"
-
-neo_value neo_vm_opt_convert(neo_context ctx, uint32_t opt, int argc,
-                             neo_value *argv) {
-  neo_value source = argv[0];
-  return source;
-}
